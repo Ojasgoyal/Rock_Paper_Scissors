fb27b7a719b5f7a533fd1fb835cb515ad68873d5
Ojas Goyal <ojasgoyal05@gmail.com>
2024-08-10 19:40:43 +0530

    improvements

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Rock , Paper , Scissors !
 
 # The most popular game coded in python .
+
+# Press q to Exit
--- rps.py
+++ rps.py
@@ -1,30 +1,30 @@
 from random import choice
 
 score = {'win' : 0 , 'loss' : 0 , 'draw' : 0}
-try:
-    while(True):
-        list1 = ["r","p","s"]
-        user = input("Rock , Paper , Scissor ! : ").lower()
-        comp = choice(list1)
-        mydict = {'r':'rock','p':'paper','s':'scissor'}
-        if(user == 'r' and comp == 'p') or (user == 'p' and comp == 's') or (user == 's' and comp == 'r'):
-            score['loss'] += 1
-            print(mydict[comp])
-            print("You Lose !")
-        elif user == comp:
-            score['draw'] += 1
-            print(mydict[comp])
-            print("Draw !")
-        elif(user == 'p' and comp == 'r') or (user == 's' and comp == 'p') or (user == 'r' and comp == 's'):
-            score['win'] += 1
-            print(mydict[comp])
-            print("You Win !")
-        elif(user == "score"):
-            print(f"Win : {score['win']}\nLoss : {score['loss']}\nDraw : {score['draw']}")
-        elif(user == 'exit'):
-            quit()
-        else:
-            pass
-except EOFError:
-    exit("BYE BYE !")
+
+
+while(True):
+    list1 = ["r","p","s"]
+    user = input("Rock , Paper , Scissor ! : ").lower()
+    if(user == 'q'):
+        break
+    if user not in list1:
+        continue
+    comp = choice(list1)
+    mydict = {'r':'rock','p':'paper','s':'scissor'}
+
+    print(f"Computer picked {mydict[comp]}" )
+
+    if(user == 'p' and comp == 'r') or (user == 's' and comp == 'p') or (user == 'r' and comp == 's'):
+        score['win'] += 1
+        print("You Win !")
+    elif user == comp:
+        score['draw'] += 1
+        print("Draw !")       
+    else:
+        score['loss'] += 1
+        print("You Lose !")   
+        
+
+print(f"Win : {score['win']}\nLoss : {score['loss']}\nDraw : {score['draw']}")
 
